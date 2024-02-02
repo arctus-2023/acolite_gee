258658deae6cb2133f06caa88b53841e0565bcf8
arctus-2023 <summer01.qiu@gmail.com>
2024-02-02 14:29:13 -0500

    raise unknown sensor exception

--- acolite/gee_arctus/reader.py
+++ acolite/gee_arctus/reader.py
@@ -27,8 +27,15 @@ class LevelTOA(object):
         self.sensor = 'S2A_MSI'
         if self.composite_scenes[0][:3] == 'S2B':
             self.sensor = 'S2B_MSI'
+        elif self.composite_scenes[0][:3] == 'S2A':
+            self.sensor = 'S2A_MSI'
         elif self.composite_scenes[0].startswith('LC08') or self.composite_scenes[0].startswith('L8'):
             self.sensor = 'L8_OLI'
+        elif self.composite_scenes[0].startswith('LC09') or self.composite_scenes[0].startswith('L9'):
+            self.sensor = 'L9_OLI'
+        else:
+            raise Exception('unknown sensor!')
+
 
 
         self.theta_v = np.asarray([float(_.split(',')[1]) for _ in _temp]).mean()
